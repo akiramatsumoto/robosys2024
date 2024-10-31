cac782670f4545e743f31d0760a1ea7c7ca6e5aa
Akira Matsumoto <akiram8427@gmail.com>
2024-10-31 12:38:53 +0900

    テスト項目の追加

--- test.bash
+++ test.bash
@@ -1,4 +1,4 @@
-#!/bin/bash
+#!/bin/bash -xv
 # SPDX-FileCopyrightText: 2024 Akira Matsumoto <akiram8427@gmail.com>
 # SPDX-License-Identifier: BSD-3-Clause
 
@@ -9,8 +9,18 @@ ng () {
 
 res=0
 
+### 通常の入力 ###
 out=$(seq 5 | ./plus)
 [ "$out" = 15 ] || ng "$LINENO"
+
+### 変な入力 ###
+out=$(echo あ | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
+
+out=$(echo | ./plus)
+[ "$?" = 1 ] || ng "$LINENO"
+[ "$out" = "" ] || ng "$LINENO"
 
 [ "$res" = 0 ] && echo OK
 exit "$res"
