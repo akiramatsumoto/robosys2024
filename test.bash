3ac120e30b9400fb5ce961ae320eca7c2fd46dd7
Akira Matsumoto <akiram8427@gmail.com>
2024-10-31 12:19:15 +0900

    change test.bash

--- test.bash
+++ test.bash
@@ -1,4 +1,6 @@
 #!/bin/bash
+# SPDX-FileCopyrightText: 2024 Akira Matsumoto <akiram8427@gmail.com>
+# SPDX-License-Identifier: BSD-3-Clause
 
 ng () {
 	echo ${1}行目が違うよ
@@ -6,8 +8,9 @@ ng () {
 }
 
 res=0
-a=山田
-[ "$a" = 上田 ] || ng "$LINENO"
-[ "$a" = 山田 ] || ng "$LINENO"
 
+out=$(seq 5 | ./plus)
+[ "$out" = 15 ] || ng "$LINENO"
+
+[ "$res" = 0 ] && echo OK
 exit "$res"
